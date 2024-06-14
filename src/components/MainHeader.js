541442df0282528304a5f928c67af58318a6c5db
Sanya <sanya@MacBook-Pro-10.local>
2024-06-14 22:53:55 +0530

    updated watchflix

---
+++ src/components/MainHeader.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const MainHeader = () => {
+  return (
+    <div className="absolute">
+        <img 
+        className="w-44"
+        src="https://cdn.cookielaw.org/logos/dd6b162f-1a32-456a-9cfe-897231c7763c/4345ea78-053c-46d2-b11e-09adaef973dc/Netflix_Logo_PMS.png"
+        alt="logo"/>
+    </div>
+  )
+}
+
+export default MainHeader
---
+++ src/components/Signin.js
@@ -0,0 +1,18 @@
+import React from "react";
+import MainHeader from "./MainHeader";
+
+const Signin = () => {
+  return (
+    <div>
+      <MainHeader />
+      <div>
+        <img
+          src="https://assets.nflxext.com/ffe/siteui/vlv3/51c1d7f7-3179-4a55-93d9-704722898999/be90e543-c951-40d0-9ef5-e067f3e33d16/IN-en-20240610-popsignuptwoweeks-perspective_alpha_website_small.jpg"
+          alt="logo"
+        />
+      </div>
+    </div>
+  );
+};
+
+export default Signin;
